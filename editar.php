6132bb96785e6874cee90305ed83661986fc1127
Matheus de Carvalho <129870244+MatheusCarvalho15@users.noreply.github.com>
2023-04-11 19:37:24 -0300

    Update editar.php

--- editar.php
+++ editar.php
@@ -18,7 +18,7 @@ $materia = $_SESSION['materia'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="able.css">
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
   
